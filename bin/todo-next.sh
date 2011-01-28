0ad1da109f2e695539814d3fe0a169839f4e7c66
steeef <steeef@gmail.com>
2011-01-28 15:01:31 -0800

    add 2nd_floor context

--- bin/todo-next.sh
+++ bin/todo-next.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$HOME/.bin/todo.sh view context | sed -n '/\-\-\-  anywhere\|pc\|office\|call\|wait  \-\-\-/,/^$/p' > $HOME/todo.txt
+$HOME/.bin/todo.sh view context | sed -n '/\-\-\-  anywhere\|pc\|office\|call\|wait|2nd_floor  \-\-\-/,/^$/p' > $HOME/todo.txt
