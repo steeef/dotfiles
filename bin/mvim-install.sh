6fc53672f4028d0f6af83f5855c4863260049186
Stephen Price <stephen@stp5.net>
2013-02-21 14:22:27 -0800

    mvim

---
+++ bin/mvim-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+MVIMREPO=git://github.com/b4winckler/macvim.git
+DOWNLOADDIR=$HOME/code/mvim
+
+/bin/mkdir -p $DOWNLOADDIR
+GITCLONED=$(cd $DOWNLOADDIR && git rev-parse --git-dir 2> /dev/null);GITCLONEDERR=$?
+if [ $GITCLONEDERR == 0 ]; then
+    cd $DOWNLOADDIR && git pull
+else
+    git clone $MVIMREPO $DOWNLOADDIR
+    cd $DOWNLOADDIR
+fi
+./configure --with-features=huge --enable-rubyinterp --enable-pythoninterp --enable-cscope && make && open MacVim/build/Release
+
