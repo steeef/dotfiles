cfc6aac3d41cc76ab1c2a75dd5e8ca64a0d94e59
Stephen Price <steeef@gmail.com>
2013-06-12 11:34:15 -0700

    fix path

--- bin/tmux_reload_todo.sh
+++ bin/tmux_reload_todo.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #
-
+TMUX=/opt/local/bin/tmux
 TMUX_SESSION_NAME=todotxt
 # Check that tmux is installed and there is a session with the expected name
-command -v tmux >/dev/null 2>&1 && \
-    (tmux ls -F '#S' | grep -q "${TMUX_SESSION_NAME}") && USE_TMUX=1
+/usr/bin/logger "todo.txt changed"
+command -v $TMUX >/dev/null 2>&1 && \
+    ($TMUX ls -F '#S' | grep -q "${TMUX_SESSION_NAME}") && USE_TMUX=1
 
 # Force Clam (within Vim) to reload todo.txt
 if [ "${USE_TMUX}" -eq "1" ]; then
-    tmux send-keys -l -t "${TMUX_SESSION_NAME}:0.0" '\r'
+    $TMUX send-keys -l -t "${TMUX_SESSION_NAME}:0.0" '\r'
+else
+    /usr/bin/logger "tmux not working"
 fi
