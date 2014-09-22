280322257014d167f956099897297b66b248b480
Stephen Price <stephen@stp5.net>
2014-09-21 21:16:42 -0700

    bootstrap

---
+++ bin/bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function ensure_link {
+    test -L "$HOME/$2" || ln -s "$HOME/.dotfiles/$1" "$HOME/$2"
+}
+
+mkdir -p "${HOME}/code"
+mkdir -p "${HOME}/bin"
+mkdir -p "${HOME}/build"
+
+ensure_link "bin"       ".bin"
+ensure_link "vimrc"     ".vimrc"
+ensure_link "vim"       ".vim"
+ensure_link "ackrc"     ".ackrc"
+ensure_link "bash"      ".bash"
+ensure_link "gemrc"     ".gemrc"
+ensure_link "gitignore" ".gitignore"
+ensure_link "gvimrc"    ".gvimrc"
+ensure_link "hgignore"  ".hgignore"
+ensure_link "irbrc"     ".irbrc"
+ensure_link "inputrc"   ".inputrc"
+ensure_link "maid"      ".maid"
+ensure_link "railsrc"   ".railsrc"
+ensure_link "tmux"      ".tmux"
+ensure_link "sshrc"     ".sshrc"
+ensure_link "screenrc"  ".screenrc"
+ensure_link "tmux"      ".tmux"
+ensure_link "tmux.conf" ".tmux.conf"
