1930d90fffcd44866615bda9765573424272d437
Stephen Price <steeef@gmail.com>
2014-11-11 12:42:58 -0800

    update for hg check

--- bin/vim-install.sh
+++ bin/vim-install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 VIMREPO=https://vim.googlecode.com/hg/
 DOWNLOADDIR=$HOME/code/vim
-HG=`which hg`
+
+command -v hg >/dev/null 2>&1 || { echo >&2 "hg not installed."; exit 1; }
 
 /bin/mkdir -p $DOWNLOADDIR
-HGCLONED=$($HG -R $DOWNLOADDIR root);HGCLONEDERR=$?
+HGCLONED=$(hg -R $DOWNLOADDIR root);HGCLONEDERR=$?
 if [ $HGCLONEDERR == 0 ]; then
-    cd $DOWNLOADDIR && $HG pull -u
+    cd $DOWNLOADDIR && hg pull -u
 else
-    $HG clone $VIMREPO $DOWNLOADDIR
+    hg clone $VIMREPO $DOWNLOADDIR
     cd $DOWNLOADDIR
 fi
 ./configure --with-features=huge --enable-rubyinterp --enable-pythoninterp --enable-cscope --prefix=$HOME && /usr/bin/make clean && /usr/bin/make && /usr/bin/make install
