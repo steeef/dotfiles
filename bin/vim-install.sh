3beebc223e0e2c2b29045285ade0c82136876a27
Stephen Price <steeef@gmail.com>
2015-03-05 17:12:10 -0800

    revert

--- bin/vim-install.sh
+++ bin/vim-install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 VIMREPO=https://vim.googlecode.com/hg/
-DOWNLOADDIR=$HOME/download/vim
+DOWNLOADDIR=$HOME/code/vim
 
 command -v hg >/dev/null 2>&1 || { echo >&2 "hg not installed."; exit 1; }
 
 /bin/mkdir -p $DOWNLOADDIR
-hg archive $VIMREPO $DOWNLOADDIR
-cd $DOWNLOADDIR
+HGCLONED=$(hg -R $DOWNLOADDIR root);HGCLONEDERR=$?
+if [ $HGCLONEDERR == 0 ]; then
+    cd $DOWNLOADDIR && hg pull -u
+else
+    hg clone $VIMREPO $DOWNLOADDIR
+    cd $DOWNLOADDIR
+fi
 ./configure --with-features=huge --enable-rubyinterp --enable-pythoninterp --enable-cscope --prefix=$HOME && /usr/bin/make clean && /usr/bin/make && /usr/bin/make install
 
