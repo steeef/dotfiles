fc648088fccc0515fe5627226e09c26174af561d
Stephen Price <stephen@stp5.net>
2015-09-03 20:52:30 -0700

    lua change, hammerspoon update

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,5 @@
+-- Lots of stuff from https://github.com/rtoshiro/hammerspoon-init
+
 --------------------------------------------------------------------------------
 -- constants
 --------------------------------------------------------------------------------
@@ -9,27 +11,223 @@ local mod = {"cmd", "alt", "shift", "ctrl"}
 hs.window.animationDuration = 0
 
 
+
+function config()
+  -- Type clipboard
+  hs.hotkey.bind(mod, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+
+  -- Start Screensaver
+  hs.hotkey.bind(mod, "0", function()
+    hs.timer.doAfter(1, function()
+      hs.caffeinate.startScreensaver()
+    end)
+  end)
+
+  -- window management
+  hs.hotkey.bind(mod, "right", function()
+    local win = hs.window.focusedWindow()
+    win:right()
+  end)
+
+  hs.hotkey.bind(mod, "left", function()
+    local win = hs.window.focusedWindow()
+    win:left()
+  end)
+
+  hs.hotkey.bind(mod, "up", function()
+    local win = hs.window.focusedWindow()
+    win:up()
+  end)
+
+  hs.hotkey.bind(mod, "down", function()
+    local win = hs.window.focusedWindow()
+    win:down()
+  end)
+
+  hs.hotkey.bind(mod, "2", function()
+    local win = hs.window.focusedWindow()
+    hs.window.fullscreenCenter(win)
+  end)
+end
+
+--------------------------------------------------------------------------------
+-- window layout stuff
+--------------------------------------------------------------------------------
+
+-- Returns the width of the smaller screen size
+-- isFullscreen = false removes the toolbar
+-- and dock sizes
+function hs.screen.minWidth(isFullscreen)
+  local min_width = math.maxinteger
+  local allScreens = hs.screen.allScreens()
+  for i, screen in ipairs(allScreens) do
+    local screen_frame = screen:frame()
+    if (isFullscreen) then
+      screen_frame = screen:fullFrame()
+    end
+    min_width = math.min(min_width, screen_frame.w)
+  end
+  return min_width
+end
+
+-- isFullscreen = false removes the toolbar
+-- and dock sizes
+-- Returns the height of the smaller screen size
+function hs.screen.minHeight(isFullscreen)
+  local min_height = math.maxinteger
+  local allScreens = hs.screen.allScreens()
+  for i, screen in ipairs(allScreens) do
+    local screen_frame = screen:frame()
+    if (isFullscreen) then
+      screen_frame = screen:fullFrame()
+    end
+    min_height = math.min(min_height, screen_frame.h)
+  end
+  return min_height
+end
+
+-- If you are using more than one monitor, returns X
+-- considering the reference screen minus smaller screen
+-- = (MAX_REFSCREEN_WIDTH - MIN_AVAILABLE_WIDTH) / 2
+-- If using only one monitor, returns the X of ref screen
+function hs.screen.minX(refScreen)
+  local min_x = refScreen:frame().x
+  local allScreens = hs.screen.allScreens()
+  if (#allScreens > 1) then
+    min_x = refScreen:frame().x + ((refScreen:frame().w - hs.screen.minWidth()) / 2)
+  end
+  return min_x
+end
+
+-- If you are using more than one monitor, returns Y
+-- considering the focused screen minus smaller screen
+-- = (MAX_REFSCREEN_HEIGHT - MIN_AVAILABLE_HEIGHT) / 2
+-- If using only one monitor, returns the Y of focused screen
+function hs.screen.minY(refScreen)
+  local min_y = refScreen:frame().y
+  local allScreens = hs.screen.allScreens()
+  if (#allScreens > 1) then
+    min_y = refScreen:frame().y + ((refScreen:frame().h - hs.screen.minHeight()) / 2)
+  end
+  return min_y
+end
+
+-- If you are using more than one monitor, returns the
+-- half of minX and 0
+-- = ((MAX_REFSCREEN_WIDTH - MIN_AVAILABLE_WIDTH) / 2) / 2
+-- If using only one monitor, returns the X of ref screen
+function hs.screen.almostMinX(refScreen)
+  local min_x = refScreen:frame().x
+  local allScreens = hs.screen.allScreens()
+  if (#allScreens > 1) then
+    min_x = refScreen:frame().x + (((refScreen:frame().w - hs.screen.minWidth()) / 2) - ((refScreen:frame().w - hs.screen.minWidth()) / 4))
+  end
+  return min_x
+end
+
+-- If you are using more than one monitor, returns the
+-- half of minY and 0
+-- = ((MAX_REFSCREEN_HEIGHT - MIN_AVAILABLE_HEIGHT) / 2) / 2
+-- If using only one monitor, returns the Y of ref screen
+function hs.screen.almostMinY(refScreen)
+  local min_y = refScreen:frame().y
+  local allScreens = hs.screen.allScreens()
+  if (#allScreens > 1) then
+    min_y = refScreen:frame().y + (((refScreen:frame().h - hs.screen.minHeight()) / 2) - ((refScreen:frame().h - hs.screen.minHeight()) / 4))
+  end
+  return min_y
+end
+
+-- Returns the frame of the smaller available screen
+-- considering the context of refScreen
+-- isFullscreen = false removes the toolbar
+-- and dock sizes
+function hs.screen.minFrame(refScreen, isFullscreen)
+  return {
+    x = hs.screen.minX(refScreen),
+    y = hs.screen.minY(refScreen),
+    w = hs.screen.minWidth(isFullscreen),
+    h = hs.screen.minHeight(isFullscreen)
+  }
+end
+
+-- +-----------------+
+-- |        |        |
+-- |        |  HERE  |
+-- |        |        |
+-- +-----------------+
+function hs.window.right(win)
+  local minFrame = hs.screen.minFrame(win:screen(), false)
+  minFrame.x = minFrame.x + (minFrame.w/2)
+  minFrame.w = minFrame.w/2
+  win:setFrame(minFrame)
+end
+
+-- +-----------------+
+-- |        |        |
+-- |  HERE  |        |
+-- |        |        |
+-- +-----------------+
+function hs.window.left(win)
+  local minFrame = (win:screen()):minFrame(false)
+  minFrame.w = minFrame.w/2
+  win:setFrame(minFrame)
+end
+
+-- +-----------------+
+-- |      HERE       |
+-- +-----------------+
+-- |                 |
+-- +-----------------+
+function hs.window.up(win)
+  local minFrame = hs.screen.minFrame(win:screen(), false)
+  minFrame.h = minFrame.h/2
+  win:setFrame(minFrame)
+end
+
+-- +-----------------+
+-- |                 |
+-- +-----------------+
+-- |      HERE       |
+-- +-----------------+
+function hs.window.down(win)
+  local minFrame = hs.screen.minFrame(win:screen(), false)
+  minFrame.y = minFrame.y + minFrame.h/2
+  minFrame.h = minFrame.h/2
+  win:setFrame(minFrame)
+end
+
+-- +------------------+
+-- |                  |
+-- |    +--------+    +--> minY
+-- |    |  HERE  |    |
+-- |    +--------+    |
+-- |                  |
+-- +------------------+
+-- Where the window's size is equal to
+-- the smaller available screen size
+function hs.window.fullscreenCenter(win)
+  local minFrame = hs.screen.minFrame(win:screen(), false)
+  win:setFrame(minFrame)
+end
+
+--------------------------------------------------------------------------------
+-- reload config
+--------------------------------------------------------------------------------
 function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
+  doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
     end
-    if doReload then
-        hs.reload()
-    end
+  end
+  if doReload then
+    hs.reload()
+  end
 end
 
 -- Reload config
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 
--- Type clipboard
-hs.hotkey.bind(mod, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
 
--- Start Screensaver
-hs.hotkey.bind(mod, "0", function()
-    hs.timer.doAfter(1, function()
-        hs.caffeinate.startScreensaver()
-    end)
-end)
+config()
