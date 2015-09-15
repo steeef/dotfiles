fa436765f0b93706f0d4ca141ff2ac29af5df7f4
Stephen Price <steeef@gmail.com>
2015-09-15 08:55:37 -0700

    add documention to vim/mvim installers

--- bin/mvim-install.sh
+++ bin/mvim-install.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+
+# Clones/updates from macvim-dev repo on GitHub, and builds with specific
+# features:
+# - ruby
+# - python
+# - cscope
+# - "huge" features
+#
+# Dependencies:
+# git
+#
+# https://github.com/b4winckler/macvim/wiki/Building
+
 MVIMREPO=https://github.com/macvim-dev/macvim.git
 DOWNLOADDIR=$HOME/code/mvim
 
 command -v git >/dev/null 2>&1 || { echo >&2 "git not installed."; exit 1; }
 
 /bin/mkdir -p $DOWNLOADDIR
+# Check if $DOWNLOADDIR is a git repo
 GITCLONED=$(cd $DOWNLOADDIR && git rev-parse --git-dir 2> /dev/null);GITCLONEDERR=$?
 if [ $GITCLONEDERR == 0 ]; then
     # Check if remote is expected repo. Remove dir if not
@@ -16,5 +30,8 @@ else
     git clone $MVIMREPO $DOWNLOADDIR
     cd $DOWNLOADDIR
 fi
-LDFLAGS=-L/usr/lib ./configure --with-features=huge --enable-rubyinterp --enable-pythoninterp --enable-cscope && make && open src/MacVim/build/Release
 
+LDFLAGS=-L/usr/lib ./configure --with-features=huge --enable-rubyinterp \
+    --enable-pythoninterp --enable-cscope \
+    && make \
+    && open src/MacVim/build/Release # copy from this directory to /Applications
--- bin/vim-install.sh
+++ bin/vim-install.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
+
+# Clones/updates from official Vim repo on GitHub, and builds with specific
+# features:
+# - ruby
+# - python
+# - cscope
+# - "huge" features
+#
+# Dependencies:
+# Debian: build-essential git ruby-dev python-dev libncurses5-dev
+# RHEL: make automake gcc-c++ git ruby-devel python-devel ncurses-devel
+# OS X: git
+
 VIMREPO=https://github.com/vim/vim.git
 DOWNLOADDIR=$HOME/code/vim
 
 command -v git >/dev/null 2>&1 || { echo >&2 "git not installed."; exit 1; }
 
 /bin/mkdir -p $DOWNLOADDIR
+# Check if $DOWNLOADDIR is a git repo
 GITCLONED=$(cd $DOWNLOADDIR && git rev-parse --git-dir);GITCLONEDERR=$?
 if [ $GITCLONEDERR == 0 ]; then
+    # Check if remote is expected repo. Remove dir if not
+    REMOTE=$(cd $DOWNLOADDIR; git remote -v | grep fetch | tr ' ' '\t' | cut -f2)
+    [ "${REMOTE}" -eq "${MVIMREPO}" ] || rm -rf $DOWNLOADDIR
     cd $DOWNLOADDIR && git pull
 else
     rm -rf $DOWNLOADDIR
     git clone $VIMREPO $DOWNLOADDIR
     cd $DOWNLOADDIR
 fi
-./configure --with-features=huge --enable-rubyinterp --enable-pythoninterp --enable-cscope --prefix=$HOME && /usr/bin/make clean && /usr/bin/make && /usr/bin/make install
-
+./configure --with-features=huge --enable-rubyinterp --enable-pythoninterp \
+    --enable-cscope --prefix=$HOME \
+    && /usr/bin/make clean \
+    && /usr/bin/make \
+    && /usr/bin/make install
