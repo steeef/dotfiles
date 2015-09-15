260cf2805eee66150d33284b1bb6b45bce94c21a
Stephen Price <stephen@stp5.net>
2015-09-15 06:35:53 -0700

    switch macvim repo to macvim-dev
    probably need to remove local copy first

--- bin/mvim-install.sh
+++ bin/mvim-install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-MVIMREPO=https://github.com/b4winckler/macvim.git
+MVIMREPO=https://github.com/macvim-dev/macvim.git
 DOWNLOADDIR=$HOME/code/mvim
+
+command -v git >/dev/null 2>&1 || { echo >&2 "git not installed."; exit 1; }
 
 /bin/mkdir -p $DOWNLOADDIR
 GITCLONED=$(cd $DOWNLOADDIR && git rev-parse --git-dir 2> /dev/null);GITCLONEDERR=$?
 if [ $GITCLONEDERR == 0 ]; then
     cd $DOWNLOADDIR && git pull
 else
+    rm -rf $DOWNLOADDIR
     git clone $MVIMREPO $DOWNLOADDIR
     cd $DOWNLOADDIR
 fi
