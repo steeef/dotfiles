80766a1b67813cab5e7bd8f1ee7c2f5dabb43be7
Stephen Price <stephen@stp5.net>
2015-09-15 07:03:38 -0700

    mvim install: check remote repo

--- bin/mvim-install.sh
+++ bin/mvim-install.sh
@@ -7,6 +7,9 @@ command -v git >/dev/null 2>&1 || { echo >&2 "git not installed."; exit 1; }
 /bin/mkdir -p $DOWNLOADDIR
 GITCLONED=$(cd $DOWNLOADDIR && git rev-parse --git-dir 2> /dev/null);GITCLONEDERR=$?
 if [ $GITCLONEDERR == 0 ]; then
+    # Check if remote is expected repo. Remove dir if not
+    REMOTE=$(cd $DOWNLOADDIR; git remote -v | grep fetch | tr ' ' '\t' | cut -f2)
+    [ "${REMOTE}" -eq "${MVIMREPO}" ] || rm -rf $DOWNLOADDIR
     cd $DOWNLOADDIR && git pull
 else
     rm -rf $DOWNLOADDIR
