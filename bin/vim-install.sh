0a25d890d4f88d524a16711721daa910c5dc9f64
Stephen Price <stephen@stp5.net>
2015-09-14 23:33:58 -0700

    install vim from github

--- bin/vim-install.sh
+++ bin/vim-install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-VIMREPO=https://vim.googlecode.com/hg/
+VIMREPO=https://github.com/vim/vim.git
 DOWNLOADDIR=$HOME/code/vim
 
-command -v hg >/dev/null 2>&1 || { echo >&2 "hg not installed."; exit 1; }
+command -v git >/dev/null 2>&1 || { echo >&2 "git not installed."; exit 1; }
 
 /bin/mkdir -p $DOWNLOADDIR
-HGCLONED=$(hg -R $DOWNLOADDIR root);HGCLONEDERR=$?
-if [ $HGCLONEDERR == 0 ]; then
-    cd $DOWNLOADDIR && hg pull -u
+GITCLONED=$(cd $DOWNLOADDIR && git rev-parse --git-dir);GITCLONEDERR=$?
+if [ $GITCLONEDERR == 0 ]; then
+    cd $DOWNLOADDIR && git pull
 else
-    hg clone $VIMREPO $DOWNLOADDIR
+    git clone $VIMREPO $DOWNLOADDIR
     cd $DOWNLOADDIR
 fi
 ./configure --with-features=huge --enable-rubyinterp --enable-pythoninterp --enable-cscope --prefix=$HOME && /usr/bin/make clean && /usr/bin/make && /usr/bin/make install
