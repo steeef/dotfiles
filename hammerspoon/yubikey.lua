77724eb3ec322bb5d59b4ccf0f85863eb973ed7a
Stephen Price <steeef@gmail.com>
2017-07-07 10:00:00 -0700

    karabiner: switch for ergodox

--- hammerspoon/yubikey.lua
+++ hammerspoon/yubikey.lua
@@ -1,3 +1,10 @@
+-- Switch between Karabiner-Elements profiles by keyboard
+
+function switchKarabinerElementsProfile(name)
+  hs.execute("~/.bin/karabiner_cli --select-profile " .. name)
+  hs.notify.show("ErgoDox", "Switching to profile", name)
+end
+
 -- from https://gist.github.com/ojkelly/45dda0a5a7066c6a79a038ece8bde55a
 local usbWatcher = nil
 
@@ -7,6 +14,7 @@ function usbDeviceCallback(data)
   -- this line will let you know the name of each usb device you connect, useful for the string match below
   -- hs.notify.show("USB", "You just connected", data["productName"])
   -- Replace "Yubikey" with the name of the usb device you want to use.
+  -- hs.notify.show("USB", "You just connected/disconnected", data["productName"])
   if string.match(data["productName"], "Yubikey NEO") then
     if (data["eventType"] == "added") then
       hs.notify.show("Yubikey", "You just connected", data["productName"])
@@ -16,6 +24,12 @@ function usbDeviceCallback(data)
         hs.application.launchOrFocus('/System/Library/Frameworks/' ..
         'ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app')
       end)
+    end
+  elseif string.match(data["productName"], "ErgoDox EZ") then
+    if (data["eventType"] == "added") then
+      switchKarabinerElementsProfile("ergodox")
+    elseif (data["eventType"] == "removed") then
+      switchKarabinerElementsProfile("default")
     end
   end
 end
