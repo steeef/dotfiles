72f4c458b7e638357a76066a820073f7a73affdf
Stephen Price <stephen@stp5.net>
2018-05-29 17:15:53 -0700

    vim install fix

--- bin/vim-install.sh
+++ bin/vim-install.sh
@@ -15,27 +15,32 @@
 VIMREPO=https://github.com/vim/vim.git
 DOWNLOADDIR=$HOME/code/vim
 
+if [ "$(uname)" = "Darwin" ]; then
+  EXTRA_ARGS="--with-python3-config-dir=/usr/local/Cellar/python/3.6.5/Frameworks/Python.framework/Versions/3.6/lib/python3.6/config-3.6m-darwin --with-python3-command=/usr/local/bin/python3"
+else
+  EXTRA_ARGS=""
+fi
+
 command -v git >/dev/null 2>&1 || { echo >&2 "git not installed."; exit 1; }
 
-/bin/mkdir -p $DOWNLOADDIR
+/bin/mkdir -p "${DOWNLOADDIR}"
 # Check if $DOWNLOADDIR is a git repo
-GITCLONED=$(cd $DOWNLOADDIR && git rev-parse --git-dir);GITCLONEDERR=$?
+(cd "${DOWNLOADDIR}" && git rev-parse --git-dir >/dev/null);GITCLONEDERR=$?
 if [ $GITCLONEDERR == 0 ]; then
-    cd $DOWNLOADDIR && git pull
+    cd "${DOWNLOADDIR}" && git pull
 else
-    rm -rf $DOWNLOADDIR
-    git clone $VIMREPO $DOWNLOADDIR
-    cd $DOWNLOADDIR
+    rm -rf "${DOWNLOADDIR}"
+    git clone "${VIMREPO}" "${DOWNLOADDIR}"
+    cd "${DOWNLOADDIR}"
 fi
 ./configure \
     --with-features=huge \
     --enable-rubyinterp \
     --disable-pythoninterp \
     --enable-python3interp \
-    --with-python3-config-dir=/usr/local/Cellar/python/3.6.5/Frameworks/Python.framework/Versions/3.6/lib/python3.6/config-3.6m-darwin \
-    --with-python3-command=/usr/local/bin/python3 \
+    ${EXTRA_ARGS} \
     --enable-cscope \
-    --prefix=$HOME \
+    --prefix="${HOME}" \
     --enable-fail-if-missing \
     && /usr/bin/make clean \
     && /usr/bin/make \
