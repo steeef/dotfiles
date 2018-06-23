f3dacf6d512c0321b509529d2030bf9f3980d5ef
Stephen Price <stephen@stp5.net>
2018-06-23 10:09:19 -0700

    update status

--- i3/status.py
+++ i3/status.py
@@ -12,7 +12,7 @@ stat.register(
     format=[("%-m/%-d %-H:%M %Z", "America/Los_Angeles"), ("%-m/%-d %-H:%M %Z", "UTC")],
 )
 
-stat.register("alsa", format=" {volume}%", format_muted=" M", on_leftclick=["mate-volume-control"])
+stat.register("alsa", format=" {volume}%", format_muted=" --", on_leftclick=["mate-volume-control"])
 
 stat.register(
     "battery",
