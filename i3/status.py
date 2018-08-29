c886b96e42798ab6938ea2f0f20eea34db2a52d5
Stephen Price <stephen@stp5.net>
2018-08-28 23:21:02 -0700

    disable log

--- i3/status.py
+++ i3/status.py
@@ -1,6 +1,6 @@
 from i3pystatus import Status
 
-stat = Status(standalone=True, logfile="/home/sprice/i3status.log")
+stat = Status(standalone=True)
 
 
 cal_cmd = "notify-send 'Calendar' \"<tt>$(cal)</tt>\""
--- i3/status.sh
+++ i3/status.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+eval "$(pyenv init -)"
 export PYENV_VERSION=i3status
 python ~/.i3/status.py
