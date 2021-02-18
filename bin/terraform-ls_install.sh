3e3608b3259db7feb2fc76e001578e31a374fcfd
Stephen Price <stephen@stp5.net>
2021-02-18 13:14:09 -0800

    update terraform-ls

--- bin/terraform-ls_install.sh
+++ bin/terraform-ls_install.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-TERRAFORM_LS_VERSION=0.8.0
+TERRAFORM_LS_VERSION=0.13.0
 
 if [ "$(uname)" = "Darwin" ]; then
   SHA256SUM="$(which gsha256sum)"
-  TERRAFORM_LS_CHECKSUM=1075f4e740656964b1e470b1799ccc5b25b3e437ca2b2ff24619bf6b32cace76
+  TERRAFORM_LS_CHECKSUM=45fafa0b61a7208c120c5faf8a16421c02aa7de1aa55d0fab61798903c69386e
   TERRAFORM_LS_URL="https://github.com/hashicorp/terraform-ls/releases/download/v${TERRAFORM_LS_VERSION}/terraform-ls_${TERRAFORM_LS_VERSION}_darwin_amd64.zip"
 else
   SHA256SUM="$(which sha256sum)"
-  TERRAFORM_LS_CHECKSUM=b82f391fc1a40a1560bd154325d720d22bf1c959af7f21fec8c9f3ec4d181969
+  TERRAFORM_LS_CHECKSUM=63fddc1d2e10008013311bad5124d10da6011b240e75059895d5b190f7dc8ea1
   TERRAFORM_LS_URL="https://github.com/hashicorp/terraform-ls/releases/download/v${TERRAFORM_LS_VERSION}/terraform-ls_${TERRAFORM_LS_VERSION}_linux_amd64.zip"
 fi
 
