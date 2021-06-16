6f416a00f9a21b5da910e317b4c65dc52afdf317
Stephen Price <stephen@stp5.net>
2021-06-16 10:50:14 -0700

    reorder

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,13 @@
-require 'paq-nvim' {
-  'savq/paq-nvim';
-  {'nvim-treesitter/nvim-treesitter', run=function() vim.cmd 'TSUpdate' end};
-}
+local api = vim.api
 
 local function map(mode, lhs, rhs, opts)
   local options = {noremap = true}
   if opts then options = vim.tbl_extend('force', options, opts) end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  api.nvim_set_keymap(mode, lhs, rhs, options)
 end
+
+require 'paq-nvim' {
+  'savq/paq-nvim';
+  {'nvim-treesitter/nvim-treesitter', run=function() vim.cmd 'TSUpdate' end};
+}
 
