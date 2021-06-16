1f6799ed862f1fa96197fb6bf3ed2584cff35e9b
Stephen Price <stephen@stp5.net>
2021-06-16 10:48:55 -0700

    add function for mapping with noremap
    https://github.com/ojroques/dotfiles/blob/master/nvim/init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,10 @@ require 'paq-nvim' {
   'savq/paq-nvim';
   {'nvim-treesitter/nvim-treesitter', run=function() vim.cmd 'TSUpdate' end};
 }
+
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
