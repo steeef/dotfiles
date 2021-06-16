8418827bdb3e2a2c4b676df307b596a6c748d526
Stephen Price <stephen@stp5.net>
2021-06-16 10:53:28 -0700

    add first mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,8 +6,17 @@ local function map(mode, lhs, rhs, opts)
   api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+-- plugins with paq
 require 'paq-nvim' {
   'savq/paq-nvim';
   {'nvim-treesitter/nvim-treesitter', run=function() vim.cmd 'TSUpdate' end};
 }
 
+
+-- mapping
+
+---- swap colon with semicolon
+map('n', ';', ':')
+map('n', ':', ';')
+map('v', ';', ':')
+map('v', ':', ';')
