370f636ae11e48193291aeaa1d5dcd9efad2397d
Stephen Price <stephen@stp5.net>
2021-11-02 12:36:41 -0700

    move theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,5 @@
-local api, cmd, g, opt = vim.api, vim.cmd, vim.g, vim.opt
-
 require('plugins')
 require('mappings')
+require('theme')
 
 -- options
-
---- colorscheme
-opt.background = "dark"
-opt.termguicolors = true
-api.nvim_command('let base16colorspace=256')
-cmd('silent! colorscheme base16-tomorrow-night')
---
+++ nvim/theme.lua
@@ -0,0 +1,7 @@
+local cmd, opt = vim.cmd, vim.opt
+
+--- colorscheme
+opt.background = "dark"
+opt.termguicolors = true
+api.nvim_command('let base16colorspace=256')
+cmd('silent! colorscheme base16-tomorrow-night')
