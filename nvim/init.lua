0de9ae694f3123a0e40380f1c4e32b5bbe9582d6
Stephen Price <stephen@stp5.net>
2021-11-02 12:34:40 -0700

    move mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,5 @@
 local api, cmd, g, opt = vim.api, vim.cmd, vim.g, vim.opt
 
-local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
 require('plugins')
 
 -- options
@@ -15,25 +9,3 @@ opt.background = "dark"
 opt.termguicolors = true
 api.nvim_command('let base16colorspace=256')
 cmd('silent! colorscheme base16-tomorrow-night')
-
--- mapping
-
---- set leader to space
-map('', '<Space>', '<Nop>', {silent = true})
-g.mapleader = " "
-g.maplocalleader = " "
-
---- swap colon with semicolon
-map('n', ';', ':')
-map('n', ':', ';')
-map('v', ';', ':')
-map('v', ':', ';')
-
---- new vertical window
-map('n', '<leader>w', '<Cmd>botright vnew<CR>', {silent = true})
-
---- window navigation with tmux-navigator
-map('n', '<C-l>', ':TmuxNavigateRight <CR>', {silent = true})
-map('n', '<C-j>', ':TmuxNavigateDown <CR>', {silent = true})
-map('n', '<C-k>', ':TmuxNavigateUp <CR>', {silent = true})
-map('n', '<C-h>', ':TmuxNavigateLeft <CR>', {silent = true})
