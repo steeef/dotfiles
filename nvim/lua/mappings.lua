5fb9cc4cf4108199642eff2d359c7dbb957b5757
Stephen Price <stephen@stp5.net>
2021-11-03 08:13:27 -0700

    simplify mapping functions

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,34 +1,53 @@
 local api, g = vim.api, vim.g
 
-local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  api.nvim_set_keymap(mode, lhs, rhs, options)
+local function map(mode, shortcut, command)
+  local options = {noremap = true, silent = true}
+  api.nvim_set_keymap(mode, shortcut, command, options)
+end
+
+local function nmap(shortcut, command)
+  map('n', shortcut, command)
+end
+
+local function imap(shortcut, command)
+  map('i', shortcut, command)
+end
+
+local function vmap(shortcut, command)
+  map('v', shortcut, command)
+end
+
+local function cmap(shortcut, command)
+  map('c', shortcut, command)
+end
+
+local function tmap(shortcut, command)
+  map('t', shortcut, command)
 end
 
 -- mapping
 
 --- set leader to space
-map('', '<Space>', '<Nop>', {silent = true})
+map('', '<Space>', '<Nop>')
 g.mapleader = " "
 g.maplocalleader = " "
 
 --- swap colon with semicolon
-map('n', ';', ':')
-map('n', ':', ';')
-map('v', ';', ':')
-map('v', ':', ';')
+nmap(';', ':')
+nmap(':', ';')
+vmap(';', ':')
+vmap(':', ';')
 
 --- new vertical window
-map('n', '<leader>w', '<Cmd>botright vnew<CR>', {silent = true})
+nmap('<leader>w', '<Cmd>botright vnew<CR>')
 
 --- window navigation with tmux-navigator
-map('n', '<C-l>', ':TmuxNavigateRight <CR>', {silent = true})
-map('n', '<C-j>', ':TmuxNavigateDown <CR>', {silent = true})
-map('n', '<C-k>', ':TmuxNavigateUp <CR>', {silent = true})
-map('n', '<C-h>', ':TmuxNavigateLeft <CR>', {silent = true})
+nmap('<C-l>', ':TmuxNavigateRight <CR>')
+nmap('<C-j>', ':TmuxNavigateDown <CR>')
+nmap('<C-k>', ':TmuxNavigateUp <CR>')
+nmap('<C-h>', ':TmuxNavigateLeft <CR>')
 
 -- Telescope
-map('n', '<leader> ', ':Telescope find_files<CR>', {silent = true})
-map('n', '<leader>b', ':Telescope buffers<CR>', {silent = true})
-map('n', '<leader>a', ':Telescope live_grep<CR>', {silent = true})
+nmap('<leader> ', ':Telescope find_files<CR>')
+nmap('<leader>b', ':Telescope buffers<CR>')
+nmap('<leader>a', ':Telescope live_grep<CR>')
