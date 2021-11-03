12468d107943be381b3cc24927714853c9cde114
Stephen Price <stephen@stp5.net>
2021-11-03 10:35:43 -0700

    add completion

---
+++ nvim/lua/autocompletion.lua
@@ -0,0 +1,31 @@
+local cmp = require('cmp')
+cmp.setup({
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+  }, {
+    { name = 'buffer' },
+  });
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+  capabilities = capabilities
+}
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -9,6 +9,7 @@ require('packer').startup(function()
 
   -- colorschemes
   use {'RRethy/nvim-base16'}
+
   use {
     'nvim-lualine/lualine.nvim',
     requires = {'kyazdani42/nvim-web-devicons', opt = true},
@@ -37,7 +38,20 @@ require('packer').startup(function()
   use {'RRethy/vim-illuminate'} -- highlight other occurrences of word under cursor
   use {'neovim/nvim-lspconfig'}
   use {'kabouzeid/nvim-lspinstall'}
-  use {'hrsh7th/nvim-compe'} -- completion
+  use {
+    'hrsh7th/nvim-cmp', -- completion
+    requires = {
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'hrsh7th/cmp-cmdline'},
+    },
+    config = function()
+      if pcall(require, 'cmp') then
+        require('autocompletion')
+      end
+    end
+  }
   use {'p00f/nvim-ts-rainbow'} -- rainbow parenthesis
   use {
     'nvim-treesitter/nvim-treesitter',
