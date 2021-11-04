f36200e0e84006693232ad75253c79bfb5a1a4d9
Stephen Price <stephen@stp5.net>
2021-11-04 10:22:34 -0700

    always enable rainbow

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -1,10 +1,8 @@
-local has_rainbow = pcall(require, 'rainbow')
-
 require('nvim-treesitter.configs').setup {
   ensure_installed = 'maintained',
   highlight = {enable = true},
   rainbow = {
-    enable = has_rainbow,
+    enable = true,
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = 500, -- Do not enable for files with more than n lines, int
   }
