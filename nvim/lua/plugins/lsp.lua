4980db1381f2d20589a87e5d8168cff7db0ed76f
Stephen Price <stephen@stp5.net>
2023-02-24 11:17:07 -0800

    lsp fixes

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -15,27 +15,37 @@ local lsp_tools = {
   "yamllint",
 }
 
+local lsps = {
+  "bashls",
+  "jsonls",
+  "lua_ls",
+  "marksman",
+  "pyright",
+  "terraformls",
+  "tflint",
+  "yamlls",
+}
+
 local lsp_plugins = {
   {
     'neovim/nvim-lspconfig',
     lazy = true,
-    dependencies = { 'mason-lspconfig.nvim', 'nlsp-settings.nvim' },
+    dependencies = { 'mason-lspconfig.nvim' },
   },
   {
     'williamboman/mason-lspconfig.nvim',
     dependencies = { 'mason.nvim' },
     cmd = { 'LspInstall', 'LspUninstall' },
+    event = { 'BufNewFile', 'BufReadPost' },
     lazy = true,
     config = function()
-      mason_lspconfig = require("mason-lspconfig")
+      local mason_lspconfig = require("mason-lspconfig")
       mason_lspconfig.setup({
-        ensure_installed = lsp_tools
+        ensure_installed = lsps
       })
       mason_lspconfig.setup_handlers({
         function (server_name)
-          require("lspconfig")[server_name].setup {
-            on_attach = require("shared").on_attach,
-          }
+          require("lspconfig")[server_name].setup {}
         end
       })
     end,
