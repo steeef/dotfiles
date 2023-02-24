1731e4eb24fee52f26f6adf59f2cc2990bf8fda6
Stephen Price <stephen@stp5.net>
2023-02-24 11:30:36 -0800

    run null-ls on inset

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -26,6 +26,8 @@ local lsps = {
   "yamlls",
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 local lsp_plugins = {
   {
     'neovim/nvim-lspconfig',
@@ -66,9 +68,11 @@ local lsp_plugins = {
   {
     'jose-elias-alvarez/null-ls.nvim',
     lazy = true,
+    event = 'InsertEnter',
     config = function()
       local null_ls = require('null-ls')
       null_ls.setup({
+        debug = true,
         sources = {
           null_ls.builtins.code_actions.gitsigns,
           null_ls.builtins.code_actions.shellcheck,
@@ -78,6 +82,19 @@ local lsp_plugins = {
           null_ls.builtins.formatting.shfmt,
           null_ls.builtins.formatting.terraform_fmt,
         },
+        -- you can reuse a shared lspconfig on_attach callback here
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+              end,
+            })
+          end
+        end,
       })
     end
   },
