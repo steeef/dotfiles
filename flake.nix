813289bd5cc4d408b78afb8e98b3d0ce067678d5
Stephen Price <stephen@stp5.net>
2023-03-13 10:19:27 -0700

    simplify

--- flake.nix
+++ flake.nix
@@ -3,25 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, flake-utils, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ]
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          homeConfigurations.myhome = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations.myhome = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
 
-            modules = [
-              ./home.nix
-            ];
-          };
-      });
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
 }
