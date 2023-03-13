74f9e07ca3b9bebdd11dfa785f8452713c0cc405
Stephen Price <stephen@stp5.net>
2023-03-13 10:00:23 -0700

    fix syntax, cleanup

--- flake.nix
+++ flake.nix
@@ -3,29 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-    }
-  }
+    };
+  };
 
-  outputs = { nixpkgs, home-manager, utils, ... }: {
+  outputs = { nixpkgs, home-manager, flake-utils, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ]
-    (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        homeConfigurations.myhome = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          homeConfigurations.myhome = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
 
-          modules = [
-            ./home.nix
-          ];
-        };
-        packages.homeConfigurations = self.homeConfigurations.${system};
-        packages.default = self.homeConfigurations.${system}.myhome.activationPackage;
-      }
-    );
-  }
+            modules = [
+              ./home.nix
+            ];
+          };
+      });
 }
