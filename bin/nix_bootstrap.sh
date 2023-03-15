d5db8f0adbfe6da9c244cfa980a71fc4c915eb34
Stephen Price <stephen@stp5.net>
2023-03-14 19:46:15 -0700

    changes

--- bin/nix_bootstrap.sh
+++ bin/nix_bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 #
 # requirements:
 # - zsh
@@ -9,35 +9,33 @@ PATH="${HOME}/bin:${HOME}/.bin:${PATH}"
 export PATH
 
 distro_and_version="$(get-distro)"
-distro_version="$(echo "${distro_and_version}" | awk '{print $2}')"
+arch="$(echo "${distro_and_version}" | awk '{print $3}')"
 
 function is_macos() {
-  [[ $distro_and_version =~ MacOS ]]
+        [[ $distro_and_version =~ MacOS ]]
 }
 
 function is_debian() {
-  [[ $distro_and_version =~ Debian ]]
+        [[ $distro_and_version =~ Debian ]]
 }
-
-arch="$(uname -m)"
 
 # install nix via https://zero-to-nix.com
 if ! command -v nix >/dev/null 2>&1; then
-  curl --proto '=https' --tlsv1.2 -sSf -L "https://install.determinate.systems/nix" | sh -s -- install
+        curl --proto '=https' --tlsv1.2 -sSf -L "https://install.determinate.systems/nix" | sh -s -- install
 fi
 
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
 if [ "${arch}" = "x86_64" ]; then
-  if is_macos; then
-    nix run ~/.dotfiles#homeConfigurations.macbook.activationPackage
-  elif is_debian; then
-    nix run ~/.dotfiles#homeConfigurations.linux.activationPackage
-  else
-    echo "ERROR: unknown distribution"
-  fi
+        if is_macos; then
+                nix run ~/.dotfiles#homeConfigurations.macbook.activationPackage
+        elif is_debian; then
+                nix run ~/.dotfiles#homeConfigurations.linux.activationPackage
+        else
+                echo "ERROR: unknown distribution"
+        fi
 elif [ "${arch}" = "aarch64" ]; then
-  echo "ERROR: aarch64 not supported (yet)"
+        echo "ERROR: aarch64 not supported (yet)"
 else
-  echo "ERROR: unknown architecture"
+        echo "ERROR: unknown architecture"
 fi
