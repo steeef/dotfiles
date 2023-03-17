a24fb32329a2e28cbba8d6a4389d4337bc61be4d
Stephen Price <stephen@stp5.net>
2023-03-17 10:47:01 -0700

    add tmux

--- home.nix
+++ home.nix
@@ -16,10 +16,13 @@
     gnugrep
     gnused
     jq
+    nil
+    nixpkgs-fmt
     reattach-to-user-namespace
     rename
     ripgrep
     ssh-copy-id
+    statix
     tree
     watch
     wget
@@ -119,5 +122,75 @@
       condition = "gitdir:~/code/work/";
       path = "~/.gitconfig-work";
     }];
+  };
+
+  programs.tmux = {
+    mouse = true;
+    newSession = true;
+    prefix = "C-Space";
+    terminal = "screen-256color";
+
+    plugins = with pkgs; [
+      tmuxPlugins.dracula
+      {
+        plugin = tmuxPlugin.resurrect;
+        extraConfig = ''
+          set -g @resurrect-capture-pane-contents 'on'
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+        '';
+      }
+      {
+        plugin = tmuxPlugin.continuum;
+        extraConfig = ''
+          set -g @continuum-boot 'on'
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '5'
+          set -g @continuum-boot-options 'iterm'
+        '';
+      }
+    ];
+
+    extraConfig = ''
+      set -g status off
+      set-option -g history-limit 30000
+      set-window-option -g automatic-rename
+      unbind C-b
+
+      # quick pane cycling
+      unbind ^A
+      bind ^A select-pane -t :.+
+
+      # use vi mode
+      setw -g mode-keys vi
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+
+      # smart pane switching with awareness of vim splits
+      # https://github.com/christoomey/vim-tmux-navigator
+      is_vim='echo "#{pane_current_command}" | grep -iqE "(^|\/)g?(view|n?vim?x?)(diff)?$"'
+      bind -n 'C-h' if-shell "$is_vim" "send-keys C-h" "select-pane -L"
+      bind -n 'C-j' if-shell "$is_vim" "send-keys C-j" "select-pane -D"
+      bind -n 'C-k' if-shell "$is_vim" "send-keys C-k" "select-pane -U"
+      bind -n 'C-l' if-shell "$is_vim" "send-keys C-l" "select-pane -R"
+      bind -n 'C-\' if-shell "$is_vim" "send-keys C-\\" "select-pane -l"
+
+      # add map for Ctrl-L to clear
+      bind C-l send-keys C-l
+
+      # use w to create panes
+      bind w split-window -h
+
+      # send a command to screen/tmux session running inside tmux (e.g., ssh session)
+      bind-key a send-prefix
+
+      # vim copy/paste
+      bind-key -T copy-mode-vi v send -X begin-selection
+      bind-key -T copy-mode-vi y send -X copy-pipe "reattach-to-user-namespace pbcopy"
+      unbind -T copy-mode-vi Enter
+      bind-key -T copy-mode-vi Enter send -X copy-pipe "reattach-to-user-namespace pbcopy"
+    '';
   };
 }
