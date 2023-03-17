4809becde58bb2ec9c8f6511ec4d17374769404b
Stephen Price <stephen@stp5.net>
2023-03-17 11:25:52 -0700

    use tmux.conf file

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
   home.username = "sprice";
   home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/sprice" else "/home/sprice";
 
@@ -156,46 +156,6 @@
       }
     ];
 
-    extraConfig = ''
-      set -g status off
-      set-option -g history-limit 30000
-      set-window-option -g automatic-rename
-      unbind C-b
-
-      # quick pane cycling
-      unbind ^A
-      bind ^A select-pane -t :.+
-
-      # use vi mode
-      setw -g mode-keys vi
-      bind h select-pane -L
-      bind j select-pane -D
-      bind k select-pane -U
-      bind l select-pane -R
-
-      # smart pane switching with awareness of vim splits
-      # https://github.com/christoomey/vim-tmux-navigator
-      is_vim='echo "#{pane_current_command}" | grep -iqE "(^|\/)g?(view|n?vim?x?)(diff)?$"'
-      bind -n 'C-h' if-shell "$is_vim" "send-keys C-h" "select-pane -L"
-      bind -n 'C-j' if-shell "$is_vim" "send-keys C-j" "select-pane -D"
-      bind -n 'C-k' if-shell "$is_vim" "send-keys C-k" "select-pane -U"
-      bind -n 'C-l' if-shell "$is_vim" "send-keys C-l" "select-pane -R"
-      bind -n 'C-\' if-shell "$is_vim" "send-keys C-\\" "select-pane -l"
-
-      # add map for Ctrl-L to clear
-      bind C-l send-keys C-l
-
-      # use w to create panes
-      bind w split-window -h
-
-      # send a command to screen/tmux session running inside tmux (e.g., ssh session)
-      bind-key a send-prefix
-
-      # vim copy/paste
-      bind-key -T copy-mode-vi v send -X begin-selection
-      bind-key -T copy-mode-vi y send -X copy-pipe "reattach-to-user-namespace pbcopy"
-      unbind -T copy-mode-vi Enter
-      bind-key -T copy-mode-vi Enter send -X copy-pipe "reattach-to-user-namespace pbcopy"
-    '';
+    extraConfig = lib.strings.fileContents ./tmux.conf;
   };
 }
