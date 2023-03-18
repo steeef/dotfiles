634720d79862e49c6cdc73313896cbd7dc9b90fe
Stephen Price <stephen@stp5.net>
2023-03-18 13:57:05 -0700

    split zsh

---
+++ nix/zsh/initExtraBeforeCompInit.zsh
@@ -0,0 +1,20 @@
+ZGEN_COMPINIT_FLAGS='-i'
+ZSH_COMPDUMP="${HOME}/.zcompdump_${ZSH_VERSION}"
+ZGEN_CUSTOM_COMPDUMP="${ZSH_COMPDUMP}"
+export ZSH_COMPDUMP
+
+# Add some completions settings
+setopt ALWAYS_TO_END     # Move cursor to the end of a completed word.
+setopt AUTO_LIST         # Automatically list choices on ambiguous completion.
+setopt AUTO_MENU         # Show completion menu on a successive tab press.
+setopt AUTO_PARAM_SLASH  # If completed parameter is a directory, add a trailing slash.
+setopt COMPLETE_IN_WORD  # Complete from both ends of a word.
+unsetopt MENU_COMPLETE   # Do not autoselect the first completion entry.
+
+# install and load zgenom
+ZGEN_DIR="${HOME}/.zgenom"
+export ZGEN_DIR
+if ! [ -d "${ZGEN_DIR}" ]; then
+  git clone "https://github.com/jandamm/zgenom.git" "${ZGEN_DIR}"
+fi
+source "${ZGEN_DIR}/zgenom.zsh"
