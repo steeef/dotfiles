2d5f1de308a1413f5d003e3ae1ec319ffd2666c5
Stephen Price <stephen@stp5.net>
2023-03-18 13:45:49 -0700

    split into separate files

---
+++ nix/git.nix
@@ -0,0 +1,66 @@
+{ ... }: {
+  programs.git = {
+    enable = true;
+    aliases = {
+      co = "checkout";
+      lg = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+      go = "!f() { git checkout -b \"$1\" 2> /dev/null || git checkout \"$1\"; }; f";
+      set-upstream = "!git branch --set-upstream-to=origin/`git symbolic-ref --short HEAD`";
+    };
+    userEmail = "stephen@stp5.net";
+    userName = "Stephen Price";
+    extraConfig = {
+      apply = {
+        whitespace = "nowarn";
+      };
+      color = {
+        diff = "auto";
+        status = "auto";
+        branch = "auto";
+      };
+      commit = {
+        verbose = true;
+      };
+      core = {
+        excludesfile = "~/.gitignore";
+        autocrlf = "input";
+      };
+      credential = {
+        helper = "osxkeychain";
+      };
+      diff = {
+        compactionHeuristic = "off";
+        indentHeuristic = "on";
+      };
+      fetch = {
+        prune = "true";
+      };
+      init = {
+        templatedir = "~/.git_template";
+        defaultBranch = "main";
+      };
+      format = {
+        pretty = "%C(yellow)%h%Creset %s %C(red)(%cr)%Creset";
+      };
+      merge = {
+        tool = "vimdiff";
+        prompt = "false";
+      };
+      pull = {
+        rebase = "false";
+      };
+      push = {
+        default = "current";
+      };
+      url = {
+        "git@github.com:" = {
+          insteadOf = "https://github.com/";
+        };
+      };
+    };
+    includes = [{
+      condition = "gitdir:~/code/work/";
+      path = "~/.gitconfig-work";
+    }];
+  };
+}
---
+++ nix/tmux.nix
@@ -0,0 +1,35 @@
+{ pkgs, lib, ... }: {
+  programs.tmux = {
+    enable = true;
+    secureSocket = if pkgs.stdenv.isDarwin then false else true;
+    mouse = true;
+    escapeTime = 0;
+    historyLimit = 30000;
+    newSession = true;
+    prefix = "C-Space";
+    terminal = "screen-256color";
+
+    plugins = with pkgs.tmuxPlugins; [
+      dracula
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-capture-pane-contents 'on'
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+        '';
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-boot 'on'
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '5'
+          set -g @continuum-boot-options 'iterm'
+        '';
+      }
+    ];
+
+    extraConfig = lib.strings.fileContents ../tmux.conf;
+  };
+}
