76fd5a6576def46e5c5218aa9167eedd92a6a7a0
Stephen Price <stephen@stp5.net>
2023-03-19 17:56:44 -0700

    use proper quotes

--- nix/home/fzf.nix
+++ nix/home/fzf.nix
@@ -2,8 +2,8 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    defaultCommand = "rg --files --no-ignore --hidden --follow -g \"!{.git,node_modules}/*\" 2> /dev/null";
-    fileWidgetCommand = "rg --files --no-ignore --hidden --follow -g \"!{.git,node_modules}/*\" 2> /dev/null";
+    defaultCommand = "rg --files --no-ignore --hidden --follow -g '!{.git,node_modules}/*' 2> /dev/null";
+    fileWidgetCommand = "rg --files --no-ignore --hidden --follow -g '!{.git,node_modules}/*' 2> /dev/null";
     changeDirWidgetCommand = "bfs ~ -nohidden -type d -printf '~/%P\\n' 2> /dev/null";
     tmux.enableShellIntegration = true;
   };
