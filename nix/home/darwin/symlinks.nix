8e3efa252be634220186e6440876a995f5ea287d
Stephen Price <stephen@stp5.net>
2023-03-21 10:05:35 -0700

    define symlink for karabiner in hm

--- nix/home/darwin/symlinks.nix
+++ nix/home/darwin/symlinks.nix
@@ -1,6 +1,12 @@
 { config, ... }: {
-  home.file.".hammerspoon" = {
-    enable = true;
-    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hammerspoon";
+  home.file = {
+    ".hammerspoon" = {
+      enable = true;
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hammerspoon";
+    };
+    ".config/karabiner" = {
+      enable = true;
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/karabiner";
+    };
   };
 }
