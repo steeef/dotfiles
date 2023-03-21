b4b10bccb9e99dfec76668b2f57da0c0e6064dcf
Stephen Price <stephen@stp5.net>
2023-03-21 14:46:57 -0700

    move zsh links

--- nix/home/symlinks.nix
+++ nix/home/symlinks.nix
@@ -8,5 +8,13 @@
       enable = true;
       source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/nvim/lua";
     };
+    ".zsh" = {
+      enable = true;
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/zsh";
+    };
+    ".p10k.zsh" = {
+      enable = true;
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/zsh/p10k.zsh";
+    };
   };
 }
