9c15b29cf7db0d38e61fe9a88f9baabb704a05ca
Stephen Price <stephen@stp5.net>
2023-03-21 16:35:16 -0700

    remove old scripts

--- bin/lock_or_sleep.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$(cat /sys/class/power_supply/AC/online)" = "0" ]; then
-  systemctl suspend
-else
-  light-locker-command -l
-fi
--- bin/screensaver.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sleep 0.5 && open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app
