6ba6a39fbab6f032e53cf5f132ab79c051f53e0c
Stephen Price <stephen@stp5.net>
2023-03-24 12:07:56 -0700

    force cp

--- nix/home/darwin/_1password.nix
+++ nix/home/darwin/_1password.nix
@@ -4,6 +4,6 @@
     # (Hopefully this requirement will be lifted by 1Password at some point)
     # NOTE we don't install `op` via nix but simply copy the binary
     $DRY_RUN_CMD mkdir -p /usr/local/bin
-    $DRY_RUN_CMD cp ${pkgs._1password}/bin/op /usr/local/bin/op
+    $DRY_RUN_CMD cp -f ${pkgs._1password}/bin/op /usr/local/bin/op 2>/dev/null
   '';
 }
