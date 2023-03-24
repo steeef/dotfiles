35cde6782ce3e426b2cb34ee0b4092928062072a
Stephen Price <stephen@stp5.net>
2023-03-24 15:07:50 -0700

    add litra control

---
+++ bin/litra_control.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# codes from https://ultracrepidarian.phfactor.net/2022/03/09/controlling-the-logitech-litra-on-macos/
+
+set -e
+
+if ! command -v hidapitester >/dev/null 2>&1; then
+  echo "ERROR: can't find hidapitester"
+  exit 1
+fi
+
+function hid() {
+  hidapitester --vidpid 046D/C900 --open --length 20 --send-output $1
+}
+
+case "${1}" in
+"on")
+  hid "0x11,0xff,0x04,0x1c,0x01"
+  ;;
+"off")
+  hid "0x11,0xff,0x04,0x1c"
+  ;;
+"glow")
+  hid "0x11,0xff,0x04,0x4c,0x00,20"
+  ;;
+"dim")
+  hid "0x11,0xff,0x04,0x4c,0x00,50"
+  ;;
+"normal")
+  hid "0x11,0xff,0x04,0x4c,0x00,70"
+  ;;
+"medium")
+  hid "0x11,0xff,0x04,0x4c,0x00,100"
+  ;;
+"bright")
+  hid "0x11,0xff,0x04,0x4c,0x00,204"
+  ;;
+"2700K")
+  hid "0x11,0xff,0x04,0x9c,10,140"
+  ;;
+"3200K")
+  hid "0x11,0xff,0x04,0x9c,12,128"
+  ;;
+"6500K")
+  hid "0x11,0xff,0x04,0x9c,25,100"
+  ;;
+*)
+  echo "ERROR: unknown code"
+  ;;
+esac
