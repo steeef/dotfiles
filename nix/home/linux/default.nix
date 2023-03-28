45ad6549f0ce3f2f154cc9e3e1ccb04e683542e5
Stephen Price <stephen@stp5.net>
2023-03-28 09:23:12 -0700

    add missing stdlib

--- nix/home/linux/default.nix
+++ nix/home/linux/default.nix
@@ -1,5 +1,9 @@
-{ ... }: {
+{ pkgs, ... }: {
   targets.genericLinux.enable = true;
+
+  home.packages = with pkgs; [
+    stdenv.cc.cc.lib
+  ];
 
   home.shellAliases = {
     hms = "home-manager switch --flake $HOME/.dotfiles#$USER@linux";
