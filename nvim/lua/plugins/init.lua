079b8333545ff4926ae6743c74f3e0500f6b9d8d
Stephen Price <stephen@stp5.net>
2023-03-29 15:15:10 -0700

    add build option

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -64,6 +64,7 @@ return {
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
+    build = ":Copilot auth",
     opts = {
       suggestion = { enabled = false },
       panel = { enabled = false },
