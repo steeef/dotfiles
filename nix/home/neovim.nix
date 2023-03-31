92c85cea946fbcef43b321d2e1e2190df22ead20
Stephen Price <stephen@stp5.net>
2023-03-30 19:38:23 -0700

    nix treesitter grammars

--- nix/home/neovim.nix
+++ nix/home/neovim.nix
@@ -18,5 +18,35 @@
       nodePackages.bash-language-server
       nodePackages.yaml-language-server
     ];
+
+    plugins = with pkgs.unstable.vimPlugins; [
+      lazy-nvim
+
+      (nvim-treesitter.withPlugins (
+        plugins: with plugins; [
+          bash
+          c
+          css
+          go
+          hcl
+          html
+          java
+          json
+          lua
+          make
+          markdown
+          markdown_inline
+          nix
+          python
+          rust
+          toml
+          tsx
+          typescript
+          vim
+          yaml
+        ]
+      ))
+    ];
   };
+  home.file."./.config/nvim/".source = ./config;
 }
