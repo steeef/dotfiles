522379757dbb726c62ea1c8f7affc4c88745240e
Stephen Price <stephen@stp5.net>
2023-08-17 09:52:30 -0700

    add treesitter opts

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-context',
+    'RRethy/nvim-treesitter-endwise',
+  },
+  opts = {
+    endwise = {enable = true},
+  }
+}
