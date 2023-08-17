9aa2123a98e589b692fa2551fe9e5786cd2e5bfc
Stephen Price <stephen@stp5.net>
2023-08-17 12:28:02 -0700

    add null-ls a different way

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -34,4 +34,13 @@ return {
       },
     },
   },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources, {
+        nls.builtins.formatting.black,
+      })
+    end,
+  }
 }
