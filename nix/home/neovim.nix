1b8c2eb6f0a0c20ae52365fd02a00ffd81212e0f
Stephen Price <stephen@stp5.net>
2023-08-18 09:41:51 -0700

    remove treesitter from nix

--- nix/home/neovim.nix
+++ nix/home/neovim.nix
@@ -15,38 +15,8 @@
       sumneko-lua-language-server
       terraform-ls
       tflint
-      tree-sitter
       nodePackages.bash-language-server
       nodePackages.yaml-language-server
-    ];
-
-    plugins = with pkgs.vimPlugins; [
-      nvim-treesitter
-      (nvim-treesitter.withPlugins (
-        plugins: with plugins; [
-          bash
-          c
-          css
-          go
-          hcl
-          html
-          java
-          json
-          lua
-          make
-          markdown
-          markdown_inline
-          nix
-          python
-          regex
-          rust
-          toml
-          tsx
-          typescript
-          vim
-          yaml
-        ]
-      ))
     ];
   };
 }
