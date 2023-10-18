dc0fc42a2b5f9546183033fcbbbaf7623d4652a4
Stephen Price <stephen@stp5.net>
2023-10-17 20:56:32 -0700

    specify explicit uid

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -50,7 +50,7 @@
   users.users.sprice = {
     isNormalUser = true;
     description = "Stephen Price";
-    uid = 1000;
+    # uid = 1000;
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       alacritty
@@ -74,7 +74,7 @@
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
       in
-      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=${config.users.users.sprice.uid},gid=100" ];
+      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100" ];
   };
 
   # Allow unfree packages
