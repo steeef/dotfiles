4f277c1d512ac4d55f1db415cab0a442fb0b9a1f
Stephen Price <stephen@stp5.net>
2023-11-01 13:40:45 -0700

    add to nixos config

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -162,5 +162,24 @@
 
   system.stateVersion = "23.05"; # Did you read the comment?
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+
+    trusted-substituters = [
+      "cache.nixos.org"
+      "nix-community.cachix.org"
+    ];
+
+    trusted-users = [
+      "root"
+      "sprice"
+    ];
+  };
+
 }
