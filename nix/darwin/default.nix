b7df593baebb2d02929ff469f85d39ecc4859ed9
Stephen Price <stephen@stp5.net>
2023-11-01 13:37:26 -0700

    add to darwin config

--- nix/darwin/default.nix
+++ nix/darwin/default.nix
@@ -2,6 +2,22 @@
   nix = {
     settings = {
       auto-optimise-store = false;
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+
+      trusted-substituters = [
+        "cache.nixos.org"
+        "nix-community.cachix.org"
+      ];
+
+      trusted-users = [
+        "root"
+        "sprice"
+      ];
+
     };
     extraOptions = ''
       experimental-features = nix-command flakes
