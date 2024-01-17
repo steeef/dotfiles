378dc5c6b2bbc2e7398bb016c3cd9b1df9682459
Stephen Price <stephen@stp5.net>
2024-01-17 10:02:47 -0800

    switch to stable

--- flake.nix
+++ flake.nix
@@ -11,8 +11,8 @@
   };
 
   inputs = {
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/master";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager";
