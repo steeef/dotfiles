dd2ab7ea7f031903e4306bbd9e742ec4cc2e21bf
Stephen Price <stephen@stp5.net>
2024-02-23 13:16:42 -0800

    linting

--- bin/nextver.sh
+++ bin/nextver.sh
@@ -4,7 +4,7 @@ RE='[^0-9]*\([0-9]*\)[.]\([0-9]*\)[.]\([0-9]*\)\([0-9A-Za-z-]*\)'
 
 step="$1"
 if [ -z "$1" ]; then
-  step=patch
+  step="patch"
 fi
 
 base="$2"
@@ -15,9 +15,9 @@ if [ -z "$2" ]; then
   fi
 fi
 
-MAJOR=$(echo $base | sed -e "s#$RE#\1#")
-MINOR=$(echo $base | sed -e "s#$RE#\2#")
-PATCH=$(echo $base | sed -e "s#$RE#\3#")
+MAJOR="$(echo "$base" | sed -e "s#$RE#\1#")"
+MINOR="$(echo "$base" | sed -e "s#$RE#\2#")"
+PATCH="$(echo "$base" | sed -e "s#$RE#\3#")"
 
 case "$step" in
 major)
@@ -34,5 +34,4 @@ patch)
   ;;
 esac
 
-echo "$MAJOR.$MINOR.$PATCH"
-
+echo "v$MAJOR.$MINOR.$PATCH"
