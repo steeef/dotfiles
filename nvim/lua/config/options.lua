40d88fb78b4579e889eea00bdc6fce08084f3f0b
Stephen Price <stephen@stp5.net>
2024-03-15 07:45:29 -0700

    turn off lsp log

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-
 local opt = vim.opt
 opt.wrap = true
 opt.conceallevel = 0
+
+-- disable LSP log to avoid infinitely growing log file
+vim.lsp.set_log_level("off")
