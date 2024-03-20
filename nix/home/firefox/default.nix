a4b348993f4f595decbe95872db5facf44e46e34
Stephen Price <stephen@stp5.net>
2024-03-20 10:46:50 -0700

    add more settings

--- nix/home/firefox/default.nix
+++ nix/home/firefox/default.nix
@@ -5,8 +5,17 @@
     profiles.nix = {
       id = 0;
       settings = {
+        "general.warnOnAboutConfig" = false;
         "privacy.trackingprotection.enabled" = true;
         "signon.rememberSignons" = false;
+
+        # disable DoH
+        "network.trr.mode" = 5;
+
+        # disable pocket
+        "extensions.pocket.enabled" = false;
+        "extensions.pocket.showHome" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
       };
       userChrome = builtins.readFile ./userChrome.css;
     };
