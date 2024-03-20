d91d68a044348be46abf874d910125c70a47edba
Stephen Price <stephen@stp5.net>
2024-03-20 12:38:57 -0700

    update theme

--- nix/home/firefox/default.nix
+++ nix/home/firefox/default.nix
@@ -23,6 +23,11 @@
         "extensions.pocket.enabled" = false;
         "extensions.pocket.showHome" = false;
         "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+
+        # theme
+        "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+        "browser.theme.content-theme" = 2;
+        "browser.theme.toolbar-theme" = 2;
       };
       userChrome = builtins.readFile ./userChrome.css;
     };
