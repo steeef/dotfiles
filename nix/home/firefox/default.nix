81b4cc5d6cdd83da6b301bcb232620b523fc1aef
Stephen Price <stephen@stp5.net>
2024-03-21 09:23:19 -0700

    add extra profille

--- nix/home/firefox/default.nix
+++ nix/home/firefox/default.nix
@@ -2,43 +2,53 @@
   programs.firefox = {
     enable = true;
     package = null;
-    profiles.nix = {
-      id = 0;
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-        add-custom-search-engine
-        clearurls
-        container-proxy
-        darkreader
-        don-t-fuck-with-paste
-        facebook-container
-        libredirect
-        multi-account-containers
-        onepassword-password-manager
-        reddit-enhancement-suite
-        sidebery
-        streetpass-for-mastodon
-        ublock-origin
-        web-archives
-      ];
-      settings = {
-        "general.warnOnAboutConfig" = false;
-        "privacy.trackingprotection.enabled" = true;
-        "signon.rememberSignons" = false;
-
-        # disable DoH
-        "network.trr.mode" = 5;
-
-        # disable pocket
-        "extensions.pocket.enabled" = false;
-        "extensions.pocket.showHome" = false;
-        "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
-
-        # theme
-        "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
-        "browser.theme.content-theme" = 2;
-        "browser.theme.toolbar-theme" = 2;
+    profiles = {
+      personal = {
+        id = 1;
       };
-      userChrome = builtins.readFile ./userChrome.css;
+      nix = {
+        id = 0;
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          add-custom-search-engine
+          clearurls
+          container-proxy
+          darkreader
+          don-t-fuck-with-paste
+          facebook-container
+          libredirect
+          multi-account-containers
+          onepassword-password-manager
+          reddit-enhancement-suite
+          sidebery
+          streetpass-for-mastodon
+          ublock-origin
+          web-archives
+        ];
+        settings = {
+          "browser.aboutConfig.showWarning" = false;
+          "browser.warnOnQuit" = false;
+          "browser.warnOnQuitShortcut" = false;
+          "privacy.trackingprotection.enabled" = true;
+          "signon.rememberSignons" = false;
+
+          # disable DoH
+          "network.trr.mode" = 5;
+
+          # disable pocket
+          "extensions.pocket.enabled" = false;
+          "extensions.pocket.showHome" = false;
+          "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+
+          # theme
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+          "browser.theme.content-theme" = 2;
+          "browser.theme.toolbar-theme" = 2;
+
+          # don't disable extensions on first run
+          "extensions.autoDisableScopes" = 0;
+        };
+        userChrome = builtins.readFile ./userChrome.css;
+      };
     };
   };
 }
