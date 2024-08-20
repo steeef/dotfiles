9847432047ebf25e5db5d9838e170a6e7021894e
Stephen Price <stephen@stp5.net>
2024-08-20 15:04:14 -0700

    update tmux options

--- nix/home/tmux/default.nix
+++ nix/home/tmux/default.nix
@@ -23,6 +23,22 @@ in {
 
     plugins = with pkgs.tmuxPlugins; [
       {
+        plugin = dracula;
+        extraConfig = ''
+          set -g @dracula-plugins "cpu-usage ram-usage time"
+
+          set -g @dracula-refresh-rate 5
+          set -g @dracula-show-left-icon session
+          set -g @dracula-show-left-sep 
+          set -g @dracula-show-right-sep 
+          set -g @dracula-show-empty-plugins false
+          set -g @dracula-military-time true
+          set -g @dracula-show-powerline true
+
+          set -g @dracula-day-month true
+        '';
+      }
+      {
         plugin = resurrect;
         extraConfig = ''
           set -g @resurrect-capture-pane-contents 'on'
@@ -41,6 +57,44 @@ in {
       }
     ];
 
-    extraConfig = lib.strings.fileContents ./extraConfig.conf;
+    extraConfig = ''
+      set-window-option -g automatic-rename
+
+      # quick pane cycling
+      unbind ^A
+      bind ^A select-pane -t :.+
+
+      # use vi mode
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+
+      # smart pane switching with awareness of vim splits
+      # https://github.com/christoomey/vim-tmux-navigator
+      is_vim='echo "#{pane_current_command}" | grep -iqE "(^|\/)g?(view|n?vim?x?)(diff)?$"'
+      bind -n 'C-h' if-shell "$is_vim" "send-keys C-h" "select-pane -L"
+      bind -n 'C-j' if-shell "$is_vim" "send-keys C-j" "select-pane -D"
+      bind -n 'C-k' if-shell "$is_vim" "send-keys C-k" "select-pane -U"
+      bind -n 'C-l' if-shell "$is_vim" "send-keys C-l" "select-pane -R"
+      bind -n 'C-\' if-shell "$is_vim" "send-keys C-\\" "select-pane -l"
+
+      # add map for Ctrl-L to clear
+      bind C-l send-keys C-l
+
+      # use w to create panes
+      bind w split-window -h
+
+      # send a command to screen/tmux session running inside tmux (e.g., ssh session)
+      bind-key a send-prefix
+
+      # vim copy/paste
+      bind-key -T copy-mode-vi v send -X begin-selection
+      bind-key -T copy-mode-vi y send -X copy-pipe "reattach-to-user-namespace pbcopy"
+      unbind -T copy-mode-vi Enter
+      bind-key -T copy-mode-vi Enter send -X copy-pipe "reattach-to-user-namespace pbcopy"
+    '';
+
+    # extraConfig = lib.strings.fileContents ./extraConfig.conf;
   };
 }
