d8f4cd3dd0ab54c04e812ba673007c39ec461f8a
Stephen Price <stephen@stp5.net>
2024-08-23 19:59:06 -0700

    update sha

--- nix/pkgs/kubectl.nix
+++ nix/pkgs/kubectl.nix
@@ -11,16 +11,10 @@
         sha256 = "d0caae91072297b2915dd65f6ef3055d27646dce821ec67d18da35ba9a8dc85b";
       })
     ];
-    x86_64-darwin = [
-      (fetchurl {
-        url = "https://dl.k8s.io/release/v${version}/bin/darwin/amd64/kubectl";
-        sha256 = "0b2b965f471768b68d7a06deb85d2d2202551db5cc62a7100bb8c309e636a717";
-      })
-    ];
     aarch64-darwin = [
       (fetchurl {
         url = "https://dl.k8s.io/release/v${version}/bin/darwin/arm64/kubectl";
-        sha256 = "a5dd031329a357e4c06fc6404a5601773bba9ada075a481ef1008a0e7aa885f5";
+        sha256 = "sha256-l4Z02mIoLaaX2InDPgzDb0t+yzpNH/c/yT5ug4d9WUU=";
       })
     ];
   };
