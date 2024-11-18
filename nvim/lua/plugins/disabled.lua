70927197482100fc807303a9b1e1ad3de96a2ce0
Stephen Price <stephen@stp5.net>
2024-11-18 14:54:03 -0800

    trying out new colorscheme

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { "folke/tokyonight.nvim", enabled = false },
-  { "catppuccin/nvim", enabled = false },
   { "williamboman/mason-lspconfig.nvim", enabled = false },
   { "williamboman/mason.nvim", enabled = false },
 }
