2e624024e0d51b73ddcfea0441e3c76c7fc87ed6
Stephen Price <stephen@stp5.net>
2024-11-18 15:19:39 -0800

    remove dracula colors

--- nvim/lua/plugins/rainbow_delimiters.lua
+++ nvim/lua/plugins/rainbow_delimiters.lua
@@ -1,13 +1,3 @@
-local colors = require('dracula').colors()
-
-vim.api.nvim_set_hl(0, 'DraculaPink',    {fg = colors.pink})
-vim.api.nvim_set_hl(0, 'DraculaOrange', {fg = colors.orange})
-vim.api.nvim_set_hl(0, 'DraculaYellow',   {fg = colors.yellow})
-vim.api.nvim_set_hl(0, 'DraculaGreen', {fg = colors.green})
-vim.api.nvim_set_hl(0, 'DraculaCyan',  {fg = colors.cyan})
-vim.api.nvim_set_hl(0, 'DraculaPurple',  {fg = colors.purple})
-vim.api.nvim_set_hl(0, 'DraculaRed',  {fg = colors.red})
-
 return {
   "HiPhish/rainbow-delimiters.nvim",
   event = "VeryLazy",
@@ -21,15 +11,6 @@ return {
       query = {
         [""] = "rainbow-delimiters",
         lua = "rainbow-blocks",
-      },
-      highlight = {
-        'DraculaPink',
-        'DraculaOrange',
-        'DraculaYellow',
-        'DraculaGreen',
-        'DraculaCyan',
-        'DraculaPurple',
-        'DraculaRed',
       },
     }
   end,
