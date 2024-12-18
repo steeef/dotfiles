040218d63834317759a20396d71f3a8e950aaf14
Stephen Price <stephen@stp5.net>
2024-12-18 07:39:50 -0800

    use Catppuccin in fzf

--- nix/home/fzf.nix
+++ nix/home/fzf.nix
@@ -4,11 +4,13 @@
     enableZshIntegration = true;
     defaultCommand = "rg --files --no-ignore --hidden --follow -g '!{.git,node_modules}/*' 2> /dev/null";
 
-    # Dracula colors
+    # Catppuccin colors
     defaultOptions = [
-      "--color=dark"
-      "--color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f"
-      "--color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7"
+      "--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796"
+      "--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6"
+      "--color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
+      "--color=selected-bg:#494d64"
+      "--multi"
     ];
     fileWidgetCommand = "rg --files --no-ignore --hidden --follow -g '!{.git,node_modules}/*' 2> /dev/null";
     changeDirWidgetCommand = "bfs ~ -nohidden -type d -printf '~/%P\\n' 2> /dev/null";
