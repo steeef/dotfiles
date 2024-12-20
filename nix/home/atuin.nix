d0c69981d8c291910e85246ce545e26a58b494f6
Stephen Price <stephen@stp5.net>
2024-12-20 09:01:38 -0800

    more atuin config

--- nix/home/atuin.nix
+++ nix/home/atuin.nix
@@ -2,9 +2,10 @@
   programs.atuin = {
     enable = true;
     settings = {
+      filter_mode = "host";
       filter_mode_shell_up_key_binding = "session";
       inline_height = 50;
-      keymap_mode = "vim-normal";
+      keymap_mode = "vim-insert";
       sync_frequency = "5m";
       sync_address = "https://atuin.steeef.net";
       update_check = false;
