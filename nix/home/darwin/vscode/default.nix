ee4b35e103b03bb7733db5a4c681ff1f7bf7ee28
Stephen Price <stephen@stp5.net>
2025-03-06 10:18:50 -0800

    fix extensions

--- nix/home/darwin/vscode/default.nix
+++ nix/home/darwin/vscode/default.nix
@@ -1,11 +1,10 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
       dracula-theme.theme-dracula
       hashicorp.terraform
-      ms-python.python
       timonwong.shellcheck
       vscodevim.vim
     ];
