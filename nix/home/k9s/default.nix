921a8655f2bed39c5f1f5ac7d6589c267f3265a5
Stephen Price <stephen@stp5.net>
2025-07-02 13:43:06 -0700

    config views for k9s

--- nix/home/k9s/default.nix
+++ nix/home/k9s/default.nix
@@ -7,11 +7,25 @@
     enable = true;
     settings = {
       k9s = {
+        defaultView = "v1/nodes";
+        readOnly = true;
         ui.skin = "catppuccin-macchiato";
       };
     };
     skins = {
       catppuccin-macchiato = ./skin-catppuccin-macchiato.yaml;
+    };
+    views = {
+      "v1/nodes" = {
+        columns = [
+          "NAME"
+          "STATUS"
+          "GROUP:.metadata.labels.tatari\\.tv/asg"
+          "VERSION"
+          "PODS"
+          "AGE"
+        ];
+      };
     };
   };
 }
