5035ad559f6acfdf009ce042888831f8825ea700
Stephen Price <stephen@stp5.net>
2025-07-10 09:41:28 -0700

    update kubectl

--- nix/pkgs/kubectl.nix
+++ nix/pkgs/kubectl.nix
@@ -3,18 +3,18 @@
   fetchurl,
   installShellFiles,
 }: let
-  version = "1.30.4";
+  version = "1.32.6";
   sources = {
     x86_64-linux = [
       (fetchurl {
         url = "https://dl.k8s.io/release/v${version}/bin/linux/amd64/kubectl";
-        sha256 = "sha256-L/0CNxK7wak5Db2MDBUgHBZaadOUeH7wPto+zLS5rAY=";
+        sha256 = "sha256-DjHr+IJXi1DlD+bEPjoOPbYfakHJze1GSFvHTQPVdus=";
       })
     ];
     aarch64-darwin = [
       (fetchurl {
         url = "https://dl.k8s.io/release/v${version}/bin/darwin/arm64/kubectl";
-        sha256 = "sha256-l4Z02mIoLaaX2InDPgzDb0t+yzpNH/c/yT5ug4d9WUU=";
+        sha256 = "sha256-ishHRzpnlN010rmAySSbed7bbiNNAP0PIjz2tnvhKZk=";
       })
     ];
   };
