d6a3454231dc910700960208969f5d6b07355036
Stephen Price <stephen@stp5.net>
2025-09-17 13:17:25 -0700

    rename plugins

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   { "folke/tokyonight.nvim", enabled = false },
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
-  { "williamboman/mason.nvim", enabled = false },
+  { "mason-org/mason-lspconfig.nvim", enabled = false },
+  { "mason-org/mason.nvim", enabled = false },
 }
