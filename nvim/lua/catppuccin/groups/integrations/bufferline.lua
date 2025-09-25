8454e13bbc9593017612c97355176c5819f7c1ad
Stephen Price <stephen@stp5.net>
2025-09-25 09:14:09 -0700

    add doc

--- nvim/lua/catppuccin/groups/integrations/bufferline.lua
+++ nvim/lua/catppuccin/groups/integrations/bufferline.lua
@@ -1,4 +1,7 @@
 -- Compatibility shim for LazyVim's outdated Catppuccin bufferline integration
 -- This provides the old path that LazyVim expects, redirecting to the new location
+--
+-- Related: https://github.com/LazyVim/LazyVim/issues/6522
+-- Remove this file when LazyVim fixes their colorscheme.lua
 
 return require("catppuccin.special.bufferline")
