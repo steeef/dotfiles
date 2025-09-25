5eee65b591be3a290a9cac17bf19efcad925e416
Stephen Price <stephen@stp5.net>
2025-09-25 09:12:44 -0700

    another dang workaround

---
+++ nvim/lua/catppuccin/groups/integrations/bufferline.lua
@@ -0,0 +1,4 @@
+-- Compatibility shim for LazyVim's outdated Catppuccin bufferline integration
+-- This provides the old path that LazyVim expects, redirecting to the new location
+
+return require("catppuccin.special.bufferline")
