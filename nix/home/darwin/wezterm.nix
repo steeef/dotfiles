20d351e0afca24935ba2381cb1f09b69e02050a3
Stephen Price <stephen@stp5.net>
2025-10-17 10:12:20 -0700

    use full window

--- nix/home/darwin/wezterm.nix
+++ nix/home/darwin/wezterm.nix
@@ -24,6 +24,12 @@
         'tmux attach -t main || tmux new -s main',
       }
       config.enable_tab_bar = false
+      config.window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+      }
       config.font_rules = {
         {
           italic = true,
