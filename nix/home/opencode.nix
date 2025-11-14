761a8324e7c2cbde170ef3844e566f760c62033a
Stephen Price <stephen@stp5.net>
2025-11-14 15:47:17 -0800

    set settings

--- nix/home/opencode.nix
+++ nix/home/opencode.nix
@@ -3,7 +3,11 @@
   pkgs,
   ...
 }: {
-  programs.opencode = {
+   programs.opencode = {
+     settings = {
+       model = "gpt-5.1";
+     };
+
     enable = true;
     package = inputs.opencode.packages.${pkgs.stdenv.hostPlatform.system}.default;
     rules = builtins.readFile ./claude/memory.md;
