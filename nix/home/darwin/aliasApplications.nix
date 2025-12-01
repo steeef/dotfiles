1a309c9463d92398595fd643086be4ed6c220316
Stephen Price <stephen@stp5.net>
2025-12-01 08:18:40 -0800

    fix list

--- nix/home/darwin/aliasApplications.nix
+++ nix/home/darwin/aliasApplications.nix
@@ -5,7 +5,7 @@
         apps = pkgs.buildEnv {
           name = "home-manager-applications";
           paths = config.home.packages;
-          pathsToLink = "/Applications";
+          pathsToLink = [ "/Applications" ];
         };
       in
       lib.hm.dag.entryAfter [ "linkGeneration" ] ''
