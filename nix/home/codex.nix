2cef2fb403fca6af88dcbc8d076ee280bbba3fad
Stephen Price <stephen@stp5.net>
2025-12-09 15:59:00 -0800

    update config

--- nix/home/codex.nix
+++ nix/home/codex.nix
@@ -8,11 +8,12 @@
     package = inputs.codex-cli.packages.${pkgs.stdenv.hostPlatform.system}.default;
     custom-instructions = builtins.readFile ./claude/memory.md;
     settings = {
-      model = "gpt-5.1-codex";
+      model = "gpt-5.1-codex-max";
       approval_policy = "on-request";
       sandbox_mode = "workspace-write";
       notice = {
         hide_gpt5_1_migration_prompt = true;
+        "hide_gpt-5.1-codex-max_migration_prompt" = true;
       };
       sandbox_workspace_write = {
         network_access = true;
